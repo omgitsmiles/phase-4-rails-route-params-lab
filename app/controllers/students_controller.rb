c4b635b101864b282680adf255743303e62f55f8
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-11-03 16:37:29 -0400

    commit tree 7cb599f40e75a419eb173c3b13135033d82330f0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,29 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+   students = if params[:name] 
+      # Student.all.filter do |student|
+      #   if params[:name] == student.first_name || params[:name] == student.last_name
+      #     return student
+      #   end
+      # end
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end
     render json: students
   end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def bshow
+    student = Student.find_by(last_name: params[:last_name])
+    render json: student
+  end
+
+
 
 end
